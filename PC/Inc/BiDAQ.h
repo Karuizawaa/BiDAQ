0c57b5c9e616b0ec91fc3e05683eb3aea977d713
NUC11PAHi5 <aldisyafriansyah@protonmail.com>
2023-09-13 17:04:00 +0700

    testing

--- PC/Inc/BiDAQ.h
+++ PC/Inc/BiDAQ.h
@@ -18,6 +18,154 @@
 #define PORT	 8080
 #define MAXLINE 1024
 
+#define GetBit(var, nbit) ((var & (1 << nbit)) != 0) // Returns true / false if bit is set
+#define ResetBit(var, nbit) (var &= ~(1 << nbit));
+#define SetBit(var, nbit) (var |= (1 << nbit))
+#define FlipBit(var, nbit) (var ^= (1 << nbit))
+
+
 int sockfd;
 struct sockaddr_in servaddr;
 
+class drDC{
+	public:
+	struct sockaddr_in alamat, cli_addr;
+	// struct timeval t1, t2;
+	long long enc;
+	char buffer[MAXLINE];
+	char terima[MAXLINE];
+	char terimaSpesifik[MAXLINE];
+	int sizeReceive;
+	//lowpass
+	float vFilt, vPrev;
+
+	std::string deviceIP, senderIP;
+	socklen_t len;
+	std::chrono::steady_clock::time_point begin;
+	std::chrono::steady_clock::time_point end;
+
+	drDC(char *IP, unsigned long port){
+		memset(&alamat,/* code */ 0, sizeof(alamat));
+		memset(&cli_addr, 0, sizeof(cli_addr));
+		deviceIP = IP;
+		drDC::alamat.sin_family = AF_INET;
+		inet_pton(AF_INET, IP, &alamat.sin_addr.s_addr);
+		alamat.sin_port = htons(port);
+		len = sizeof(alamat);
+		
+	}
+
+	/* Send UDP */
+	void send(int soket, std::string pesan){
+		sendto(soket, pesan.c_str(), strlen(pesan.c_str()),
+			MSG_CONFIRM, (const struct sockaddr *) &alamat,
+				len);
+	}
+	void drive(int cps){
+		send(sockfd, std::to_string(cps));
+	}
+	/* Receive UDP */
+	void receive(int soket){
+		do{
+
+			socklen_t sender_addr_len = sizeof(cli_addr);
+			sizeReceive = recvfrom(soket, (char *)buffer, MAXLINE,
+						0, ( struct sockaddr *) &cli_addr,
+						&sender_addr_len);
+			buffer[sizeReceive] = '\0';
+			senderIP = inet_ntoa(cli_addr.sin_addr);
+		}
+		while(senderIP != deviceIP);
+
+		strncpy(terima, buffer, MAXLINE);
+        enc = atoi(terima);
+	}
+    operator long int (){
+		receive(sockfd);
+        return(atoi(terima));
+    }
+
+};
+
+class DAQ{
+	public:
+	struct sockaddr_in alamat, cli_addr;
+	// struct timeval t1, t2;
+	char buffer[MAXLINE];
+	char terima[6];
+	uint16_t dataOUTPUT;
+	uint16_t dataINPUT;
+	int sizeReceive;
+
+	std::string deviceIP, senderIP;
+	socklen_t len;
+	std::chrono::steady_clock::time_point begin;
+	std::chrono::steady_clock::time_point end;
+
+	DAQ(char *IP, unsigned long port){
+		memset(&alamat, 0, sizeof(alamat));
+		memset(&cli_addr, 0, sizeof(cli_addr));
+		deviceIP = IP;
+		DAQ::alamat.sin_family = AF_INET;
+		inet_pton(AF_INET, IP, &alamat.sin_addr.s_addr);
+		alamat.sin_port = htons(port);
+		len = sizeof(alamat);
+		
+	}
+	
+	/* Send UDP */ //send 2 byte
+	void send(int soket, uint16_t pesan){
+		sendto(soket, (const uint16_t*)pesan, sizeof(pesan),
+			MSG_CONFIRM, (const struct sockaddr *) &alamat,
+				len);
+	}
+	/* Receive UDP */
+	void receive(int soket){
+		do{
+
+			socklen_t sender_addr_len = sizeof(cli_addr);
+			sizeReceive = recvfrom(soket, (uint16_t *)dataINPUT, sizeof(dataINPUT),
+						0, ( struct sockaddr *) &cli_addr,
+						&sender_addr_len);
+			senderIP = inet_ntoa(cli_addr.sin_addr);
+		}
+		while(senderIP != deviceIP);
+	}
+
+    bool digRead(uint8_t pin){
+        receive(sockfd);
+		if(GetBit(dataINPUT, pin-1)){
+			return 1;
+		}
+		else return 0;
+    }
+
+    void digWrite(uint8_t pin, bool STATE){
+		STATE ? SetBit(dataOUTPUT, pin-1) : ResetBit(dataOUTPUT, pin-1);
+		send(sockfd, dataOUTPUT);
+    }
+};
+
+void startUDP(int udpPort){
+    // Creating socket file descriptor
+	if ( (sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0 ) {
+		perror("socket creation failed");
+		exit(EXIT_FAILURE);
+	}
+	
+	memset(&servaddr, 0, sizeof(servaddr));
+
+
+	// Filling server information
+	servaddr.sin_family = AF_INET; // IPv4
+	servaddr.sin_addr.s_addr = INADDR_ANY;
+	servaddr.sin_port = htons(udpPort);
+	
+	// Bind the socket with the server address
+	if ( bind(sockfd, (const struct sockaddr *)&servaddr,
+			sizeof(servaddr)) < 0 )
+	{
+		perror("bind failed");
+		exit(EXIT_FAILURE);
+	}
+}
--- baseDAQ/baseDAQ.ino
+++ baseDAQ/baseDAQ.ino
@@ -1,4 +1,4 @@
-/*
+  /*
  * Created by ArduinoGetStarted.com
  *
  * This example code is in the public domain
@@ -12,40 +12,58 @@
 
 #define pinB3 8
 
-uint16_t input;
 
 
 // INPUT
-uint8_t pin_input[10] = {2, 46, 3, 44, 18, 42, 19, 41, 31, 33};
-#define IN1 2
-#define IN2 46
-#define IN3 3
-#define IN4 44
-#define IN5 18
-#define IN6 42
-#define IN7 19
-#define IN8 41
-#define IN9 31 
-#define IN10 33
+uint8_t pin_input[16] = {31, 33, 35, 37, 39, 41, 43, 45, 47, 49, 22, 24, 26, 28, 30, 32};
+//PullUP
+#define IN1 31
+#define IN2 33
+#define IN3 35
+#define IN4 37
+#define IN5 39
+#define IN6 41
+#define IN7 49
+#define IN8 45
+#define IN9 47 
+#define IN10 49
+
+#define IN11 22
+#define IN12 24
+#define IN13 26
+#define IN14 28
+#define IN15 30
+#define IN16 32
 
 #define INMaxPin 16
 uint16_t dataInputPin;
 
 //OUT
-uint8_t pin_output[10] = {13,12,11,9,3,25,23,4,27,29};
-#define OUT1 13
-#define OUT2 12
-#define OUT3 11
-#define OUT4 9
-#define OUT5 3
-#define OUT6  25
-#define OUT7 23
-#define OUT8 4
-#define OUT9  27
-#define OUT10 29
+uint8_t pin_output[16] = {23, 3, 25, 27, 4, 29, 5, 6, 7, 8, 9, 11, 12, 13, 34, 36};
+#define OUT1 23
+#define OUT2 3
+#define OUT3 25
+#define OUT4 27
+#define OUT5 4
+#define OUT6 29
+#define OUT7 5
+#define OUT8 6
+#define OUT9 7
+#define OUT10 8
+#define OUT11 9
+#define OUT12 11
+#define OUT13 12
+#define OUT14 13
+#define OUT15 34
+#define OUT16 36
 
 #define OUTMaxPin 16
 uint16_t dataOutputPin;
+
+#define GetBit(var, nbit) ((var & (1 << nbit)) != 0) // Returns true / false if bit is set
+#define ResetBit(var, nbit) (var &= ~(1 << nbit));
+#define SetBit(var, nbit) (var |= (1 << nbit))
+#define FlipBit(var, nbit) (var ^= (1 << nbit))
 
 // replace the MAC address below by the MAC address printed on a sticker on the Arduino Shield 2
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
@@ -64,16 +82,22 @@ uint16_t terima;
 char headStr[20];
 
 void setup() {
-  pinMode(IN1, INPUT);
-  pinMode(IN2, INPUT);
-  pinMode(IN3, INPUT);
-  pinMode(IN4, INPUT);
-  pinMode(IN5, INPUT);
-  pinMode(IN6, INPUT);
-  pinMode(IN7, INPUT);
-  pinMode(IN8, INPUT);
-  pinMode(IN9, INPUT);
-  pinMode(IN10, INPUT);
+  pinMode(IN1, INPUT_PULLUP);
+  pinMode(IN2, INPUT_PULLUP);
+  pinMode(IN3, INPUT_PULLUP);
+  pinMode(IN4, INPUT_PULLUP);
+  pinMode(IN5, INPUT_PULLUP);
+  pinMode(IN6, INPUT_PULLUP);
+  pinMode(IN7, INPUT_PULLUP);
+  pinMode(IN8, INPUT_PULLUP);
+  pinMode(IN9, INPUT_PULLUP);
+  pinMode(IN10, INPUT_PULLUP);
+  pinMode(IN11, INPUT);
+  pinMode(IN12, INPUT);
+  pinMode(IN13, INPUT);
+  pinMode(IN14, INPUT);
+  pinMode(IN15, INPUT);
+  pinMode(IN16, INPUT);
 
   pinMode(OUT1, OUTPUT);
   pinMode(OUT2, OUTPUT);
@@ -85,6 +109,12 @@ void setup() {
   pinMode(OUT8, OUTPUT);
   pinMode(OUT9, OUTPUT);
   pinMode(OUT10, OUTPUT);
+  pinMode(OUT11, OUTPUT);
+  pinMode(OUT12, OUTPUT);
+  pinMode(OUT13, OUTPUT);
+  pinMode(OUT14, OUTPUT);
+  pinMode(OUT15, OUTPUT);
+  pinMode(OUT16, OUTPUT);
   // initialize the Ethernet shield using the static IP address:
   Ethernet.begin(mac, ip, myDns, gateway, subnet);
 
@@ -95,19 +125,21 @@ void setup() {
 void loop() {
   if(int n = Udp.parsePacket()){
     Udp.read(packetBuffer,6);  // buffer to hold incoming packet,
-    packetBuffer[n] = '\0';
-    uint16_t outputVal = (packetBuffer[1] << 8 | packetBuffer[2]);
-    for(uint8_t i=0; i <= OUTMaxPin; i++){
-      digitalWrite(pin_output[i], outputVal & (1<<i));
+    uint16_t outputVal = (packetBuffer[0] << 8 | packetBuffer[1]);
+    for(uint8_t i=0; i <= OUTMaxPin - 1; i++){
+      digitalWrite(pin_output[i], GetBit(outputVal, i));
     }
   }
   for(uint8_t i = 0; i <= INMaxPin-1; i++){
-    if(digitalRead(pin_input) == 1){
-      input |= 1 << i;
+    if(digitalRead(pin_input[i]) == 1){
+      SetBit(dataInputPin, i);
     }
     else{
-      input &= ~(1 << i);
+      ResetBit(dataInputPin, i);
     }
   }
-  Udp.write(input);
+  Udp.beginPacket(IPAddress(192, 168, 0, 55), 5555);
+  Udp.write(dataInputPin);
+  Udp.endPacket();
+  delay(1);
 }
